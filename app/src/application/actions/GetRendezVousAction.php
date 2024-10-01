cefc2d054b4cf8237347bcd7a444f020e3240529
Romain-Pailler <romain.pailler77@gmail.com>
2024-10-01 16:28:08 +0200

    endpoint getrdv fini

--- app/src/application/actions/GetRendezVousAction.php
+++ app/src/application/actions/GetRendezVousAction.php
@@ -3,28 +3,29 @@ namespace toubeelib\application\actions;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use toubeelib\core\repositoryInterfaces\RdvRepositoryInterface;
+use toubeelib\core\services\rdv\ServiceRendezVousInterface;
 
 class GetRendezVousAction extends AbstractAction
 {
-    private RdvRepositoryInterface $rdvRepo;
-    public function __construct(RdvRepositoryInterface $rdvRepo)
+    private ServiceRendezVousInterface $rdvRepo;
+
+    public function __construct(ServiceRendezVousInterface $rdvRepo)
     {
-        $this->$rdvRepo = $rdvRepo;
+        $this->rdvRepo = $rdvRepo;
     }
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
-        $idRdv = $args['id'];
+        $idRdv = $args['ID-RDV'];
 
         try {
             $rdv = $this->rdvRepo->getRendezvousById($idRdv);
             $res = ["Rendez-Vous" =>[
-                "id" => ID,
-                "id_praticient" => praticien,
-                "id_patient" => patient,
-                "specialite_praticien" => specialite_label,
-                "horaire" => date,
-                "statut" => statut,
+                "id" => $rdv->ID,
+                "id_praticient" => $rdv->praticien,
+                "id_patient" => $rdv->patient,
+                "specialite_praticien" => $rdv->specialite_label,
+                "horaire" => $rdv->date,
+                "statut" => $rdv->statut,
             ],"links" => [
                 "self"=> [
                     "href"=> "/rdvs/" . $rdv->ID
@@ -43,7 +44,8 @@ class GetRendezVousAction extends AbstractAction
                 ],
                 ]];
             $response->getBody()->write(json_encode($res));
-            return response->withHeader('Content-Type','application/json');
+            return $response->withHeader('Content-Type','application/json')
+            ->withStatus(200);
         } catch (\Throwable $th) {
             //throw $th;
         }
